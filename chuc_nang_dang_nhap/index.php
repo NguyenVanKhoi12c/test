d73e31240b5c54c42cf1dc4724a90edb14640b9a
NguyenVanKhoi12c <nguyenvankhoi12cltv@gmail.com>
2019-12-04 13:17:30 +0700

    first

---
+++ chuc_nang_dang_nhap/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once "Form_Login.php"
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container"></div>
+<div class="login"></div>
+<input type="text" name="username" size="30" placeholder="ten dang nhap"/>
+<input type="password" name="password" size="30" placeholder="mat khau"/>
+<input type="submit" value="dang nhap" >
+</body>
+</html>
